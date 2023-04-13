fb1f6ab914639eea2be743e20f4327374b41e1e0
petercarr26 <pcarr1@villanova.edu>
2023-04-13 19:57:45 -0400

    Edited to get figma page

--- VUHousing/screens/HomeScreen.tsx
+++ VUHousing/screens/HomeScreen.tsx
@@ -26,11 +26,14 @@ export default function HomeScreen({navigation}) {
 
     return (
       <View>
-          <Text style={{textAlign: 'center', marginVertical: 20, fontFamily: 'AlNile-Bold', fontSize: 40}}>VUHousing</Text>
-        <Button title='Login' 
-        onPress={()=>navigation.navigate("LoginScreen")}></Button>
-        <Button title='Sign Up' 
-        onPress={()=>navigation.navigate("Signup")}></Button>
+          
+          <Text style={{textAlign: 'center', marginVertical: 20, fontFamily: 'AlNile-Bold', fontSize: 40}}>Welcome to Nova House!</Text>
+          <Text style={{textAlign: 'center', marginVertical: 20, fontFamily: 'AlNile', fontSize: 20}}>Click ‘Search Houses” to start looking for a house or apartment</Text>
+          <Text style={{textAlign: 'center', marginVertical: 20, fontFamily: 'AlNile', fontSize: 20}}>Click “Create Listing” to enter information about the place you live in</Text>
+        <Button title='Search Houses' 
+        onPress={()=>navigation.navigate("FindListing")}></Button>
+        <Button title='Create Listing' 
+        onPress={()=>navigation.navigate("AddListing")}></Button>
       </View>
     );
   }
