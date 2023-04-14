9aca2da2704ae812e87a811c1c197e8eb7c2763e
Rowan Dillon <rdillon2@villanova.edu>
2023-04-14 14:30:35 -0400

    Update AddListing.tsx

--- VUHousing/screens/AddListing.tsx
+++ VUHousing/screens/AddListing.tsx
@@ -26,39 +26,44 @@ import {
 export default function AddListing({navigation}) {
 
   return (
-    <View>
-        <Text>Create a listing</Text>
-        <Text>Address</Text>
+    <View style={styles.container}>
+        <Text style={styles.header}>Create a listing</Text>
+
+        <Text style={styles.titles}>Address</Text>
         <TextInput
-          style={styles.addressInput}
-          // onChangeText={onChangeNumber}
-          // value={number}
+          style={styles.input}
           placeholder="800 E Lancaster Ave, Villanova, PA 19085"
           keyboardType="email-address" />
-        <Text>Bedrooms</Text>
+          
+          <View style={styles.row}>
+            <Text style={styles.rowTitles}>Bedrooms</Text>
+            <Text style={styles.rowTitles}>Bathrooms</Text>  
+          </View>
+
+          <View style={styles.row}>
+            <TextInput
+            style={styles.BBRInput}
+            placeholder="3"
+            keyboardType="numeric"/>
+            <TextInput
+            style={styles.BBRInput}
+            placeholder="2.5"
+            keyboardType="numeric"/>
+          </View>  
+        
+        <Text style={styles.titles}>Type of house</Text>
         <TextInput
           style={styles.input}
-          // onChangeText={onChangeNumber}
-          // value={number}
-          placeholder="3"
-          keyboardType="numeric"/>
-        <Text>Bathrooms</Text>
+          placeholder="ex: apartment, house, town-home"
+          keyboardType="email-address" />
+        <Text style={styles.titles}>Monthly Price</Text>
         <TextInput
           style={styles.input}
-          // onChangeText={onChangeNumber}
-          // value={number}
-          placeholder="2.5"
-          keyboardType="numeric"/>
-        <Text>Type of house</Text>
-        <Text>Monthly Price</Text>
-        <TextInput
-          style={styles.input}
-          // onChangeText={onChangeNumber}
-          // value={number}
           placeholder="$1,700"
           keyboardType="default"/>
-          <Button title='Submit' 
-          onPress={()=>navigation.navigate("AddListing")}></Button>
+          <Button title='Submit'
+            color='#001E58'
+          onPress={()=>navigation.navigate("HomeScreen")}></Button>
     </View>
   );
 }
@@ -85,24 +90,53 @@ export default function AddListing({navigation}) {
 
       const styles = StyleSheet.create({
         container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      addressInput: {
-        borderWidth: 1,
-        minWidth: 20,
-        borderColor: '#777',
-        padding: 8,
-        margin: 10,
-        width: 200,
-    },
-        input: {
-          borderWidth: 1,
-          borderColor: '#777',
-          padding: 8,
-          margin: 10,
-          width: 200,
-    }
+          flex: 1, 
+          alignItems: 'center',
+          marginTop:60,
+        },
+          row: {
+            flexDirection: 'row', 
+            alignSelf: 'center',
+          },
+          header: {
+            fontSize: 40,
+            margin: 10,
+            alignSelf: "center",
+            fontFamily: "Tahoma",
+            color: "#292828",
+          },
+          titles: {
+            fontSize: 25,
+            margin: 10,
+            alignSelf: "center",
+            fontFamily: "Georgia",
+          },
+          rowTitles: {
+            fontSize: 25,
+            margin: 10,
+            marginRight:20,
+            marginLeft:20,
+            alignSelf: "center",
+            fontFamily: "Georgia",
+          },
+          BBRInput: {
+            borderWidth: 1,
+            borderRadius: 15,
+            borderColor: 'black',
+            padding: 8,
+            marginLeft: 30,
+            marginRight: 30,
+            width: 100,
+            backgroundColor: "#D9D9D9",
+          },
+          input: {
+            alignSelf: "center",
+            borderRadius: 15,
+            borderWidth: 1,
+            borderColor: 'black',
+            backgroundColor: "#D9D9D9",
+            padding: 8,
+            margin: 10,
+            width: 300,
+          },
   });
