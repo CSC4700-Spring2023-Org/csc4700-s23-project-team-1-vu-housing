ebc4f70d05c1eb456339d6d416adf1000f8fd3b8
petercarr26 <pcarr1@villanova.edu>
2023-04-13 20:06:27 -0400

    Styles for buttons

--- VUHousing/screens/HomeScreen.tsx
+++ VUHousing/screens/HomeScreen.tsx
@@ -28,10 +28,10 @@ export default function HomeScreen({navigation}) {
       <View>
           
           <Text style={{textAlign: 'center', marginVertical: 20, fontFamily: 'AlNile-Bold', fontSize: 40}}>Welcome to Nova House!</Text>
-          <Text style={{textAlign: 'center', marginVertical: 20, fontFamily: 'AlNile', fontSize: 20}}>Click ‘Search Houses” to start looking for a house or apartment</Text>
+          <Text style={{textAlign: 'center', marginVertical: 20, fontFamily: 'AlNile', fontSize: 20}}>Click "Search Houses” to start looking for a house or apartment</Text>
           <Text style={{textAlign: 'center', marginVertical: 20, fontFamily: 'AlNile', fontSize: 20}}>Click “Create Listing” to enter information about the place you live in</Text>
         <Button title='Search Houses' 
-        onPress={()=>navigation.navigate("FindListing")}></Button>
+        onPress={()=>navigation.navigate("FindListing")} style={{alignItems: 'center', justifyContent: 'center', paddingVertical: 12, paddingHorizontal: 32, borderRadius: 4, elevation: 3, backgroundColor: 'black',}}></Button>
         <Button title='Create Listing' 
         onPress={()=>navigation.navigate("AddListing")}></Button>
       </View>
