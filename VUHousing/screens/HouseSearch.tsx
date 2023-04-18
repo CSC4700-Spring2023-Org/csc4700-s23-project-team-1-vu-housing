4e6f694b22071a4c5e6858fab8808a480674f3e3
Jimmy Gamboli <jimmygamboli@gmail.com>
2023-04-18 14:08:56 -0400

    Updated House Search.tsx
    Can now read from a database

--- VUHousing/screens/HouseSearch.tsx
+++ VUHousing/screens/HouseSearch.tsx
@@ -11,6 +11,7 @@ import {
   Text,
   useColorScheme,
   View,
+  FlatList,
 } from 'react-native';
 
 import {
@@ -22,19 +23,52 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 import TableExample from '../components/DataTable';
+import firestore from '@react-native-firebase/firestore';
 
 
 function HouseSearch({navigation}) {
 
+  const [loading, setLoading] = useState(true); // Set loading to true on component mount
+  const [users, setUsers] = useState([]); // Initial empty array of users
+
+  const subscriber = firestore()
+    .collection('Houses')
+    .onSnapshot(querySnapshot => {
+      const users = [];
+
+      querySnapshot.forEach(documentSnapshot => {
+        users.push({
+          key: documentSnapshot.id,
+          address:documentSnapshot.data().Address,
+          beds:documentSnapshot.data().beds
+        });
+      });
+
+      setUsers(users);
+      setLoading(false);
+    });
+
+
     return (
     <View>
-          
           <Text style={{textAlign: 'center', marginVertical: 20, fontFamily: 'AlNile-Bold', fontSize: 40}}>House Search</Text>
           
         <Button title='Filter' 
          style={{alignItems: 'left', justifyContent: 'center', paddingVertical: 12, paddingHorizontal: 3, borderRadius: 4, elevation: 3, backgroundColor: 'black',}}></Button>
-         <TableExample />
+         
+
+         <FlatList
+          data={users}
+          renderItem={({ item }) => (
+          <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>User ID: {item.id}</Text>
+          <Text>User Name: {item.address}</Text>
+        </View>
+      )}
+    />
       </View>   
+
+      
     );
   }
 
