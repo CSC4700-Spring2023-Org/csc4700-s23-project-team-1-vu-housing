60c85af375f8a926aab056698aa933d238f4f2c7
petercarr26 <pcarr1@villanova.edu>
2023-09-13 15:42:17 -0400

    Basically complete welcome screen
    Need new fonts

--- VUHousing/screens/WelcomeScreen.tsx
+++ VUHousing/screens/WelcomeScreen.tsx
@@ -30,17 +30,24 @@ export default function WelcomeScreen({navigation}) {
 
 
     return (
+
       <NativeBaseProvider>
-          
+        
          <Box flex={1} bg="#ffffff" alignItems="center">
-          <View id="LogoBand" style={{backgroundColor:'#0085FF',width:Dimensions.get('screen').width,alignItems:'center',marginTop:150}}>
+          <Box marginTop="20"  width="75%" alignItems="center">
+          <Text fontSize="4xl" bold>Nova House</Text>
+        </Box>
+          <View id="LogoBand" style={{backgroundColor:'#0085FF',width:Dimensions.get('screen').width,alignItems:'center',marginTop:50}}>
          <Image
          style={{height:125,width:125}}
          source = {require('VUHousing/images/Logo.png')}
          />
         </View>
-        <Text style={{fontFamily:"AlNile-Bold",fontSize:50,color:"#001F58"}}>Nova House</Text>
+        
         <Button onPress={()=>navigation.navigate("LoginScreen")} bgColor="#001F58" margin="5" size="lg" w="200" h="75" borderRadius="5">Login</Button>
+        <Box marginTop="1"  width="75%" alignItems="center">
+          <Text fontSize="lg" color="#C0C0C0" italic>Don't have an account yet?</Text>
+        </Box>
         <Button onPress={()=>navigation.navigate("Signup")} bgColor="#001F58" margin="5" size="lg" w="200" h="75" borderRadius="5">Sign Up</Button>
       </Box>
       </NativeBaseProvider>
