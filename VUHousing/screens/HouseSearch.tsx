fba0a14b8497765afd6891fb487252137551922f
Rowan Dillon <rdillon2@villanova.edu>
2023-09-20 13:48:31 -0400

    Added StreeView photos
    guh

--- VUHousing/screens/HouseSearch.tsx
+++ VUHousing/screens/HouseSearch.tsx
@@ -1,81 +1,118 @@
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import {
-    Button,
-  SafeAreaView,
+  Button,
   ScrollView,
-  VirtualizedList,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Image,
   FlatList,
-  
 } from 'react-native';
-
-
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import HouseTable from '../components/HouseTable';
 import firestore from '@react-native-firebase/firestore';
 import { DataTable } from 'react-native-paper';
-import { SectionList } from 'native-base';
 
+function HouseSearch({ navigation }) {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
 
-function HouseSearch({navigation}) {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await firestore().collection('Houses').get();
+        const userData = [];
 
-  const [loading, setLoading] = useState(true); // Set loading to true on component mount
-  const [users, setUsers] = useState([]); // Initial empty array of users
+        querySnapshot.forEach((doc) => {
+          userData.push({ id: doc.id, ...doc.data() });
+        });
 
-  
+        setUsers(userData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-  const events = firestore().collection('Houses')
-  events.get().then((querySnapshot) => {
-      const user = []
-      querySnapshot.forEach((doc) => {
-         user.push({ id: doc.id, ...doc.data() })
-      })
-      setUsers(user)
-   })
-    
+    fetchData();
+  }, []);
 
+  return (
+    <ScrollView>
+      <Text style={styles.title}>House Search</Text>
 
-    return (
-    <View>
-      <ScrollView>
-          <Text style={{textAlign: 'center', marginVertical: 20, fontFamily: 'AlNile-Bold', fontSize: 40}}>House Search</Text>
-          
-        <Button title='Filter' 
-         style={{alignItems: 'left', justifyContent: 'center', paddingVertical: 12, paddingHorizontal: 3, borderRadius: 4, elevation: 3, backgroundColor: 'black',}}></Button>
-         <HouseTable></HouseTable>
-       
-        <FlatList
-          data={users}
-          renderItem={({ item }) => (
-         <DataTable.Row onPress={()=>navigation.navigate("HomeInfo",{docID:item.id})}>
-          <DataTable.Cell>{item.Address}</DataTable.Cell>
-          <DataTable.Cell>{item.Beds}</DataTable.Cell>
-          <DataTable.Cell>{item.Baths}</DataTable.Cell>
-          <DataTable.Cell>{item.Price}</DataTable.Cell>
-         </DataTable.Row>
-      )}
-    />
-        
-      </ScrollView>
-    </View>   
+      <Button
+        title="Filter"
+        style={styles.filterButton}
+        onPress={() => {
+          // Implement filter logic here
+        }}
+      />
 
-      
-    );
-  }
+      <View style={styles.banner}>
+        <Text style={styles.label}>Address</Text>
+        <Text style={styles.label}>Beds</Text>
+        <Text style={styles.label}>Baths</Text>
+        <Text style={styles.label}>Price</Text>
+        <Text style={styles.label}>StreetView</Text>
+      </View>
 
+      <FlatList
+        data={users}
+        renderItem={({ item }) => (
+          <DataTable.Row
+            onPress={() => navigation.navigate('HomeInfo', { docID: item.id })}
+          >
+            <DataTable.Cell>{item.Address}</DataTable.Cell>
+            <DataTable.Cell>{item.Beds}</DataTable.Cell>
+            <DataTable.Cell>{item.Baths}</DataTable.Cell>
+            <DataTable.Cell>{item.Price}</DataTable.Cell>
+            <DataTable.Cell>
+              <View style={styles.container}>
+                <Image source={{ uri: item.StreetView }} style={styles.image} />
+              </View>
+            </DataTable.Cell>
+          </DataTable.Row>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    marginVertical: 20,
+    fontFamily: 'AlNile-Bold',
+    fontSize: 40,
+  },
+  filterButton: {
+    alignItems: 'left',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 3,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  banner: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8,
+    paddingHorizontal: 16,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 50, // Adjust the width and height as needed
+    height: 50,
+  },
+});
 
 export default HouseSearch;
