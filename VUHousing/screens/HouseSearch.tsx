a3abe85c45aa558fe3a170765d46630a7d0f21b7
Rowan Dillon <rdillon2@villanova.edu>
2023-09-24 20:26:30 -0400

    Added animations to filter buttons
    asdf

--- VUHousing/screens/HouseSearch.tsx
+++ VUHousing/screens/HouseSearch.tsx
@@ -31,108 +31,182 @@ import * as Animatable from 'react-native-animatable';
 import { useNavigation } from '@react-navigation/native';
 
 function HouseSearch({ navigation }) {
-  const [loading, setLoading] = useState(true); // Set loading to true on component mount
-  const [users, setUsers] = useState([]); // Initial empty array of users
-  const [address, setAddress] = useState([]);
-  const [beds, setBeds] = useState("");
-  const [baths, setBaths] = useState("");
-  const [price, setPrice]=useState("")
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [beds, setBeds] = useState('');
+  const [baths, setBaths] = useState('');
+  const [price, setPrice] = useState('');
 
-  
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await firestore().collection('Houses').get();
+        const userData = [];
 
-  const events = firestore().collection('Houses')
+        querySnapshot.forEach((doc) => {
+          userData.push({ id: doc.id, ...doc.data() });
+        });
+
+        setUsers(userData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false); // Handle the error gracefully
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const FilterQuery = () => {
+    console.log('BEDS' + beds);
+    console.log('BATHS' + baths);
+    console.log('Price' + price);
+
+    // Implement your filtering logic here using beds, baths, and price state variables
+  };
+
+  const events = firestore().collection('Houses');
   events.get().then((querySnapshot) => {
-      const user = []
-      querySnapshot.forEach((doc) => {
-         user.push({ id: doc.id, ...doc.data() })
-      })
-      setUsers(user)
-   })
+    const user = [];
+    querySnapshot.forEach((doc) => {
+      user.push({ id: doc.id, ...doc.data() });
+    });
+    setUsers(user);
+  });
 
-
-
-   function clearFilters(){
-    setBeds("");
-    setBaths("");
-    setPrice("");
-   }
- //TODO: DP Just check if the text values are nonzero and if they are then query based on non-ZeroInputs
- //TODO: Rewrite this function by using the above code to query the Houses Collection. Reference
- // The code in checkLogin() on LoginScreen.tsx. Might need to make this an async function
-   function FilterQuery(){
-    console.log("BEDS"+beds)
-    console.log("BATHS"+baths)
-    console.log("{Price}"+price)
-
-   }
-    
+  const clearFilters = () => {
+    setBeds('');
+    setBaths('');
+    setPrice('');
+  };
 
   return (
     <NativeBaseProvider>
-
-
       <View>
-      <Box alignItems="center" marginTop="5" marginBottom="5" >
-        <Text fontSize="4xl" bold>House Search</Text>
-      </Box>
-            
-     
-          <Box flexDirection={'row'} w='99%'>
-            <Input placeholder='Beds' w='33%' onChangeText={(val) => setBeds((val))} value={String(beds)}></Input>
-            <Input placeholder='Baths' w='33%' onChangeText={(val) => setBaths((val))} value={String(baths)}></Input>
-            <Input placeholder='Price' w='33%' onChangeText={(val) => setPrice((val))} value={String(price)}></Input>
-          </Box>
-          <Box flexDirection='row' alignItems="center" alignSelf='center'>
-            <Button title='filter' bgColor="#001F58" size="sm" w="100" alignSelf={'center'} borderRadius="50" _text={{ color: '#FFFFFF' }} onPress={() => FilterQuery()} >Filter</Button>
-            <Button title='clear' bgColor="#001F58" size="sm" w="100" alignSelf={'center'} borderRadius="50" _text={{ color: '#FFFFFF' }} onPress={() => clearFilters()} >Clear</Button>
-          </Box>
-          <HouseTable></HouseTable>
+        <Box alignItems="center" marginTop="5" marginBottom="5">
+          <Text color="#001F58" fontSize="3xl" bold>
+            House Search
+          </Text>
+          <CoolButton onPress={FilterQuery} clearFilters={clearFilters} isLoading={loading} />
+        </Box>
 
-          <FlatList
-          style={{height:'65%'}}
+        <Box flexDirection={'row'} w="99%">
+          <Input
+            placeholder="Beds"
+            w="33%"
+            onChangeText={(val) => setBeds(val)}
+            value={beds}
+          />
+          <Input
+            placeholder="Baths"
+            w="33%"
+            onChangeText={(val) => setBaths(val)}
+            value={baths}
+          />
+          <Input
+            placeholder="Price"
+            w="33%"
+            onChangeText={(val) => setPrice(val)}
+            value={price}
+          />
+        </Box>
+
+        {/* Render your HouseTable and other components here */}
+
+        <FlatList
+          style={{ height: '65%' }}
           data={users}
           renderItem={({ item }) => (
-          <DataTable.Row onPress={()=>navigation.navigate("HomeInfo",{docID:item.id})}>
-          <DataTable.Cell>{item.Address}</DataTable.Cell>
-          <DataTable.Cell>{item.Beds}</DataTable.Cell>
-          <DataTable.Cell>{item.Baths}</DataTable.Cell>
-          <DataTable.Cell>{item.Price}</DataTable.Cell>
-          </DataTable.Row>
-        )}
-        keyExtractor={(item) => item.id}
-      />
+            <DataTable.Row onPress={() => navigation.navigate('HomeInfo', { docID: item.id })}>
+              <DataTable.Cell>{item.Address}</DataTable.Cell>
+              <DataTable.Cell>{item.Beds}</DataTable.Cell>
+              <DataTable.Cell>{item.Baths}</DataTable.Cell>
+              <DataTable.Cell>{item.Price}</DataTable.Cell>
+            </DataTable.Row>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+
+        <View>
+          <BackButton text="Go Back" />
+        </View>
+
       </View>
-    </NativeBaseProvider >
+    </NativeBaseProvider>
   );
 }
 
+const CoolButton = ({ onPress, clearFilters, isLoading }) => {
+  const [isLoadingFilter, setIsLoadingFilter] = useState(false); // State for the Filter button
+  const [isLoadingClear, setIsLoadingClear] = useState(false);   // State for the Clear Filters button
 
+  const buttonColorFilter = isLoadingFilter ? '#b2d7e6' : '#007aff'; // Light blue when loading, dark blue when not
+  const buttonColorClear = isLoadingClear ? '#b2d7e6' : '#007aff';
 
-const styles = StyleSheet.create({
-  filterButton: {
-    justifyContent: 'center',
-    borderRadius: 4,
-    elevation: 3,
-    color: 'grey',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    width: 70, // Adjust the width and height as needed
-    height: 50,
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 10,
-    paddingHorizontal: 16,
-  },
-});
+  const handlePressFilter = () => {
+    if (isLoadingFilter) {
+      return;
+    }
+    setIsLoadingFilter(true);
+
+    setTimeout(() => {
+      setIsLoadingFilter(false);
+      if (onPress) {
+        onPress();
+      } else {
+        console.log('Filter Clicked!');
+      }
+    }, 1000); // Replace with your actual async call
+  };
+
+  const handlePressClear = () => {
+    if (isLoadingClear) {
+      return;
+    }
+    setIsLoadingClear(true);
+
+    setTimeout(() => {
+      setIsLoadingClear(false);
+      clearFilters(); // Call clearFilters function when Clear Filters button is pressed
+      console.log('Clear Filters Clicked!');
+    }, 1000); // Replace with your actual async call
+  };
+
+  return (
+    <View>
+      <Box flexDirection='row' alignItems="center" alignSelf='center'>
+        <Animatable.View animation={isLoadingFilter ? 'swing' : undefined}>
+          <Button
+            style={{ backgroundColor: buttonColorFilter }}
+            mode="contained"
+            onPress={handlePressFilter}
+            disabled={isLoadingFilter}
+          >
+            {isLoadingFilter ? (
+              <ActivityIndicator size="small" color="white" />
+            ) : (
+              <Text style={{ color: 'white' }}>Filter</Text>
+            )}
+          </Button>
+        </Animatable.View>
+        <Animatable.View animation={isLoadingClear ? 'swing' : undefined}>
+          <Button
+            style={{ backgroundColor: buttonColorClear }}
+            mode="contained"
+            onPress={handlePressClear}
+            disabled={isLoadingClear}
+          >
+            {isLoadingClear ? (
+              <ActivityIndicator size="small" color="white" />
+            ) : (
+              <Text style={{ color: 'white' }}>Clear Filters</Text>
+            )}
+          </Button>
+        </Animatable.View>
+      </Box>
+    </View>
+  );
+};
 
 export default HouseSearch;
-
